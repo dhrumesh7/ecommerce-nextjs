64d4af1a51039db35b1f608d8d47ebcea2aa4fdc
dhrumesh <dhrumesh@solvative.com>
2023-04-21 08:36:51 +0530

    prod change

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   rewrites: async () => {
     return [{
       source: '/backend/:slug*',
-      destination: 'http://localhost:4020/:slug*'
+      destination: 'http://3.111.148.12:4020/:slug*'
     }]
   },
   env: {
     GOOGLE_CLIENT_ID: `695977102477-tt3qn74pjctsp71ahvppegk18jsm60q8.apps.googleusercontent.com`,
     GOOGLE_CLIENT_SECRET: `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`,
-    BACKEND_URL: 'http://localhost:4020',
+    BACKEND_URL: 'http://3.111.148.12:4020',
     SECRET: `@!Ecom_dev@NexT@%$!+*&Dk`,
     RAZORPAY_KEY: `rzp_test_wSKSrOLwNIS37y`,
     BASE_IMAGE: `https://arya-ecom.s3.ap-south-1.amazonaws.com`,
